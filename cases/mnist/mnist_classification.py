1e2d0b28fc983600534a966667fe5457d3598e00
donRumata03 <donrumata03@gmail.com>
2024-08-14 20:30:35 +0300

    Fix the number of postprocessing layers

--- cases/mnist/mnist_classification.py
+++ cases/mnist/mnist_classification.py
@@ -175,7 +175,7 @@ def build_mnist_cls(save_path, dataset_cls, conv_is_kan=False, linear_is_kan=Fal
 
     if linear_is_kan:
         fc_layers_pool = [LayersPoolEnum.kan_linear, ]
-        min_fc_layers = 1
+        min_fc_layers = 2
         max_fc_layers = 3
     else:
         fc_layers_pool = [LayersPoolEnum.linear, ]
