Deep dive into framework to fix validation score computation

cases/mnist/mnist_classification.py
@@ -134,8 +134,8 @@ def build_mnist_cls(save_path, dataset_cls, conv_is_kan=False, linear_is_kan=Fal
     epochs = 50
     optimization_epochs = 30
     num_of_generations = 6
-    initial_population_size = 5
-    max_population_size = 5
+    initial_population_size = 2
+    max_population_size = 2
     color_mode = 'grayscale'
 
     history_path_instead_of_evolution = None  # For evolution
@@ -143,10 +143,10 @@ def build_mnist_cls(save_path, dataset_cls, conv_is_kan=False, linear_is_kan=Fal
 
     set_root(project_root())
     task = Task(TaskTypesEnum.classification)
-    raw_accuracy = MetricsRepository().metric_by_id(ClassificationMetricsEnum.accuracy)
+    objective_function = MetricsRepository().metric_by_id(ClassificationMetricsEnum.accuracy)
 
-    def objective_function(g: NasGraph):
-        return -raw_accuracy(g)
+    # def objective_function(g: NasGraph):
+    #     return -raw_accuracy(g)
 
     input_channels = _get_image_channels_num(color_mode)
 
@@ -167,9 +167,10 @@ def build_mnist_cls(save_path, dataset_cls, conv_is_kan=False, linear_is_kan=Fal
 
     dataset_path = project_root() / "cases/mnist"
 
+    eager = True
     if dataset_cls is [EuroSAT, ]:
         dataset_train, dataset_test = random_split(
-            EuroSAT(root=dataset_path, transform=transform, target_transform=one_hot_encode, eager=True,
+            EuroSAT(root=dataset_path, transform=transform, target_transform=one_hot_encode, eager=eager,
                     cache_before_transform=True),
             [.7, .3]
         )
@@ -182,10 +183,10 @@ def build_mnist_cls(save_path, dataset_cls, conv_is_kan=False, linear_is_kan=Fal
         assert num_classes == len(dataset_train.classes)
     elif dataset_cls in [CachedMNIST, CachedFashionMNIST]:
         dataset_train = dataset_cls(root=dataset_path, train=True, transform=transform, target_transform=one_hot_encode,
-                                    eager=True,
+                                    eager=eager,
                                     cache_before_transform=False)
         dataset_test = dataset_cls(root=dataset_path, train=False, transform=transform, target_transform=one_hot_encode,
-                                   eager=True,
+                                   eager=eager,
                                    cache_before_transform=False)
         assert num_classes == len(dataset_train.classes)
 
@@ -261,7 +262,7 @@ def build_mnist_cls(save_path, dataset_cls, conv_is_kan=False, linear_is_kan=Fal
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     model_trainer = ModelConstructor(model_class=NASTorchModel, trainer=NeuralSearchModel,
                                      device=device,
-                                     loss_function=CrossEntropyLoss(), optimizer=AdamW)
+                                     loss_function=CrossEntropyLoss(), optimizer=AdamW, metrics=[accuracy_score])
 
     basic_graph_time = get_time_from_graph(generate_basic_kkan(input_channels, 5),
                                            [image_side_size, image_side_size, input_channels],

nas/model/pytorch/base_model.py
@@ -300,19 +300,30 @@ class NASTorchModel(torch.nn.Module):
 
     def validate(self, val_data: DataLoader, loss_fn, device, **kwargs) -> Dict:
         self.eval()
+        self.set_device(device)
         metrics_to_calc = kwargs.get('metrics')
-        metrics = {'val_loss': 0}
+        # Would do if there weren't cpu←—→gpu transitions
+        # if metrics_to_calc is None:
+        #     metrics_to_calc = {"loss": loss_fn}
+
+        if metrics_to_calc is None:
+            metrics = {'val_loss': 0}
+        else:
+            metrics = {}
+
         for batch_id, (features_batch, targets_batch) in enumerate(val_data):
             features_batch, targets_batch = features_batch.to(device), targets_batch.to(device)
             outputs = self.__call__(features_batch)
+            major_outputs = torch.argmax(outputs, dim=-1)  # Softmax is omitted
             loss = loss_fn(outputs, targets_batch)
-            metrics['val_loss'] += loss.detach().cpu().item()
+            if metrics_to_calc is None:
+                metrics['val_loss'] += loss.detach().cpu().item()
             if metrics_to_calc:
                 for metric_name, metric_func in metrics_to_calc.items():
                     if metrics.get(f'val_{metric_name}') is None:
                         metrics[f'val_{metric_name}'] = 0
-                    metrics[f'val_{metric_name}'] += metric_func(outputs.detach().cpu().numpy(),
-                                                                 targets_batch.detach().cpu().numpy())
+                    metrics[f'val_{metric_name}'] += metric_func(major_outputs.detach().cpu().numpy(),
+                                                                 torch.argmax(targets_batch, dim=-1).detach().cpu().numpy())
 
         metrics = {key: val / len(val_data) for key, val in metrics.items()}
         return metrics